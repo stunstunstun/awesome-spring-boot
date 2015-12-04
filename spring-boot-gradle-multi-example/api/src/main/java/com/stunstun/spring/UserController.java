979ab56762b2f69baaf7306389f55221c791573f
stunstun <wjdsupj@gmail.com>
2015-12-04 17:49:18 +0900

    Update build.gradle

---
+++ spring-boot-gradle-multi-example/api/src/main/java/com/stunstun/spring/UserController.java
@@ -0,0 +1,23 @@
+package com.stunstun.spring;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author stunstun(minhyuck.jung@nhnent.com)
+ *
+ */
+@RestController
+public class UserController {
+
+	@RequestMapping(value = "/users/{name}", method = RequestMethod.GET)
+	public ResponseEntity<User> getUser(@PathVariable String name) {
+		User user = new User();
+		user.setName(name);
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+}
