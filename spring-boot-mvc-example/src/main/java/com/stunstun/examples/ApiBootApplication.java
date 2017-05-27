aabe61232333e8e812023f6b2ed67acad0793fd4
stunstun <wjdsupj@gmail.com>
2017-05-27 20:57:16 +0700

    Add spring-boot-mvc-example

---
+++ spring-boot-mvc-example/src/main/java/com/stunstun/examples/ApiBootApplication.java
@@ -0,0 +1,12 @@
+package com.stunstun.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBootApplication.class, args);
+    }
+}
---
+++ spring-boot-mvc-example/src/main/java/com/stunstun/examples/Application.java
@@ -0,0 +1,13 @@
+package com.stunstun.examples;
+
+public class Application {
+    private String applicationId;
+
+    public void setApplicationId(String applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public String getApplicationId() {
+        return applicationId;
+    }
+}
