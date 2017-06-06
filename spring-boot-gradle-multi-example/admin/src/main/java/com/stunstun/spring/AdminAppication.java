d4ae385384270e356ec7f0f3505e5a3a2d14fe26
stunstun <wjdsupj@gmail.com>
2017-06-06 22:23:37 +0700

    Update spring-boot-gradle-multi-example

---
+++ spring-boot-gradle-multi-example/admin/src/main/java/com/stunstun/spring/AdminAppication.java
@@ -0,0 +1,16 @@
+package com.stunstun.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author minhyeok
+ *
+ */
+@SpringBootApplication
+public class ApiApplication {
+	
+	public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
